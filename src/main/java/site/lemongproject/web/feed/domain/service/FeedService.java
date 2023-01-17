4eeae109c39223f64233d5c4487a6106ebf53aba
minseok <mins5714@gmail.com>
2023-01-18 00:10:20 +0900

    1.17 feed

--- src/main/java/site/lemongproject/web/feed/domain/service/FeedService.java
+++ src/main/java/site/lemongproject/web/feed/domain/service/FeedService.java
@@ -2,7 +2,14 @@ package site.lemongproject.web.feed.domain.service;
 
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+
 @Transactional
 public interface FeedService  {
+
+    // feed 게시글 가져오기
+    public ArrayList selectFeed();
+
+
 
 }
--- src/main/java/site/lemongproject/web/feed/domain/service/FeedServiceImpl.java
+++ src/main/java/site/lemongproject/web/feed/domain/service/FeedServiceImpl.java
@@ -5,4 +5,5 @@ import org.springframework.stereotype.Service;
 @Service
 public class FeedServiceImpl implements FeedService{
 
+    private FeedDao feedDao;
 }
