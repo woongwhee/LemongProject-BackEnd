eb6659a0254144e40fe0b3d32f867aeb86238cf5
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2023-01-18 00:56:32 +0900

    메일보내기기능 완성

--- src/main/java/site/lemongproject/web/template/controller/TemplateInsertController.java
+++ src/main/java/site/lemongproject/web/template/controller/TemplateInsertController.java
@@ -3,7 +3,9 @@ package site.lemongproject.web.template.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-import site.lemongproject.common.response.ResponseEntity;
+import site.lemongproject.common.response.ResponseBody;
+import site.lemongproject.common.response.ResponseBuilder;
+import site.lemongproject.web.feed.domain.vo.Feed;
 import site.lemongproject.web.member.model.vo.Member;
 import site.lemongproject.web.template.model.vo.Template;
 import site.lemongproject.web.template.service.TemplateService;
@@ -14,18 +16,17 @@ import site.lemongproject.web.template.service.TemplateService;
 public class TemplateInsertController {
     final private TemplateService templateService;
     @GetMapping("/")
-    public @ResponseBody ResponseEntity<Template> load(@SessionAttribute("loginUser") Member loginMember){
+    public ResponseBody<Template> load( Member loginMember){
         Template cur=templateService.loadInsertPage(loginMember.getUserNo());
-
-        ResponseEntity<Template> response =new ResponseEntity<>();
-
+        ResponseBody<Template> responseBody=new ResponseBody<>();
+        responseBody.setCode("2000");
+        responseBody.setMessage("SUCCESS");
+        ResponseBody<Template> response= ResponseBuilder.success(cur);
         return response;
     }
     @GetMapping(value = "/save")
-    public @ResponseBody ResponseEntity<Integer> save(@SessionAttribute("loginUser") Member loginUser){
-
-
-        ResponseEntity<Integer> response =new ResponseEntity<>();
+    public @org.springframework.web.bind.annotation.ResponseBody ResponseBody<Integer> save(@SessionAttribute("loginUser") Member loginUser){
+        ResponseBody<Integer> response =new ResponseBody<>();
         return response;
     }
 
