b9682d3b1accb96d95e31b3d31b227a74182700b
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2023-01-18 02:14:08 +0900

    병합전커밋

--- src/main/java/site/lemongproject/web/feed/domain/dao/FeedDao.java
+++ src/main/java/site/lemongproject/web/feed/domain/dao/FeedDao.java
@@ -1,13 +1,17 @@
 package site.lemongproject.web.feed.domain.dao;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 
 @Repository
+@RequiredArgsConstructor
 public class FeedDao {
+    final SqlSession sqlSession;
 
     public ArrayList selectFeed(){
-
+        return null;
     }
 }
--- src/main/java/site/lemongproject/web/feed/domain/service/FeedServiceImpl.java
+++ src/main/java/site/lemongproject/web/feed/domain/service/FeedServiceImpl.java
@@ -1,9 +1,16 @@
 package site.lemongproject.web.feed.domain.service;
 
 import org.springframework.stereotype.Service;
+import site.lemongproject.web.feed.domain.dao.FeedDao;
+
+import java.util.ArrayList;
 
 @Service
 public class FeedServiceImpl implements FeedService{
-
     private FeedDao feedDao;
+
+    @Override
+    public ArrayList selectFeed() {
+        return null;
+    }
 }
