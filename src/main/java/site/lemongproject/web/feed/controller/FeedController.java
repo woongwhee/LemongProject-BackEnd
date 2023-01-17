a001cdee4d12ba14d91d60bc6ef00850345d8cc3
minseok <mins5714@gmail.com>
2023-01-17 17:50:20 +0900

    1.17 feed

---
+++ src/main/java/site/lemongproject/web/feed/controller/FeedController.java
@@ -0,0 +1,19 @@
+package site.lemongproject.web.feed.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import site.lemongproject.web.feed.domain.service.FeedService;
+
+@RequestMapping("/feed")
+@Controller
+public class FeedController {
+
+    private FeedService feedService;
+
+
+//    public FeedController(FeedService feedService){
+//        this.feedService = feedService;
+//        // gd
+//    }
+
+}
