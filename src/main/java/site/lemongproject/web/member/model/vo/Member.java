a9550f57e2340d298696c7974850a52a7f9d609e
miseon-park <72717093+miseon-park@users.noreply.github.com>
2023-01-18 00:55:03 +0900

    미선 - 유저 관련

--- src/main/java/site/lemongproject/web/member/model/vo/Member.java
+++ src/main/java/site/lemongproject/web/member/model/vo/Member.java
@@ -4,9 +4,11 @@ import lombok.Data;
 import site.lemongproject.common.type.SocialType;
 @Data
 public class Member {
-    private int userNo;
-    private String email;
-    private String userPwd;
-    private SocialType socialType;
-    private String accessToken;
+    private int userNo; // 회원 번호
+    private String email; // 이메일
+    private String userNic; // 회원 닉네임
+    private String userPwd; // 비밀번호
+    private SocialType socialType; // 소셜 로그인 타입
+    private String accessToken; // 소셜 로그인 접근
+    private int status; // 회원 상태
 }
