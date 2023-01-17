2d741c5a5d577971736fa9d45756bc99acd4f32b
minseok <mins5714@gmail.com>
2023-01-17 22:35:05 +0900

    1.17 feed

--- src/main/java/site/lemongproject/web/feed/controller/FeedController.java
+++ src/main/java/site/lemongproject/web/feed/controller/FeedController.java
@@ -1,18 +1,35 @@
 package site.lemongproject.web.feed.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import site.lemongproject.web.feed.domain.service.FeedService;
+import site.lemongproject.web.feed.domain.vo.Feed;
+
+import java.util.ArrayList;
 
 @RequestMapping("/feed")
 @Controller
 public class FeedController {
 
+
     private FeedService feedService;
 
     public FeedController(FeedService feedService){
         this.feedService = feedService;
-
     }
+
+
+
+    @RequestMapping("feed/main")
+    public String feedMain(Model model){
+        ArrayList<Feed> list = feedService.selectFeed();
+        model.addAttribute("list",list);
+        return "feed/feedListView";
+    }
+
+    @RequestMapping("feed/insert")
+    public
+
+
+
 }
