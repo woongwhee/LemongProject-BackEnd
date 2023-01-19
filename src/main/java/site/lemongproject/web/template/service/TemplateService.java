1c3e74d3aa0cf63c37bab38a7267a1b652c5c7be
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2023-01-19 21:13:12 +0900

    마이바티스 오류수정

--- src/main/java/site/lemongproject/web/template/service/TemplateService.java
+++ src/main/java/site/lemongproject/web/template/service/TemplateService.java
@@ -1,9 +1,9 @@
 package site.lemongproject.web.template.service;
 
-import site.lemongproject.web.template.model.vo.Template;
+import site.lemongproject.web.template.model.dto.Template;
 
 public interface TemplateService {
     Template loadInsertPage(int memberNo);
 
-    int upload(int userNo);
+    int saveTemplate(int userNo);
 }
