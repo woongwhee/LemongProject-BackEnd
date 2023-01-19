e966abd680d6e5ab3402a5e2bf78a3b93bed851f
SongJiho1998 <106515028+SongJiho1998@users.noreply.github.com>
2023-01-19 17:47:53 +0900

    커밋

--- src/main/java/site/lemongproject/web/member/model/service/MemberServiceImpl.java
+++ src/main/java/site/lemongproject/web/member/model/service/MemberServiceImpl.java
@@ -8,8 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import site.lemongproject.web.member.model.dao.MemberDao;
 import site.lemongproject.web.member.model.vo.Member;
+import site.lemongproject.web.member.model.vo.Profile;
+import site.lemongproject.web.photo.model.vo.Photo;
 
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -28,5 +32,27 @@ public class MemberServiceImpl implements MemberService {
 
         return loginUser;
     }
+
+    public List<Profile> selectMyProList(){
+        return memberDao.selectMyProList();
+    }
+
+    public List<Member> selectUser(){
+        return memberDao.selectUser();
+    }
+
+    public int updateUser(String nickName){
+        return memberDao.updateUser(nickName);
+    }
+
+    public int updateComment(String comment){
+        return memberDao.updateComment(comment);
+    }
+
+    public int insertUserProfile(Photo p, String webPath, String serverFolderPath){
+        return memberDao.insertUserProfile(p , webPath , serverFolderPath);
+    }
+
+
 
 }
