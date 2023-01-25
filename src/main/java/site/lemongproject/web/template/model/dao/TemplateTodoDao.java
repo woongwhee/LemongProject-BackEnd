febad7950a851b90ee6ea680e566a3548b5911b0
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2023-01-25 15:25:44 +0900

    탬플릿 투두 매퍼 작성완료

--- src/main/java/site/lemongproject/web/template/model/dao/TemplateTodoDao.java
+++ src/main/java/site/lemongproject/web/template/model/dao/TemplateTodoDao.java
@@ -1,13 +1,20 @@
 package site.lemongproject.web.template.model.dao;
 
+import site.lemongproject.web.template.model.dto.Template;
 import site.lemongproject.web.template.model.dto.TemplateTodo;
 
 import java.util.List;
 
 public interface TemplateTodoDao {
-    List<TemplateTodo> findMany(long templateNo);
+    List<TemplateTodo> findByTemplate(int templateNo);
+    TemplateTodo findOne(long tpTodoNo);
+
     int insertOne(TemplateTodo templateTodo);
-    int deleteOne(long templateTodoNo);
+
     int updateOne(TemplateTodo templateTodo);
+    //삭제후 value조정
+    int afterDelete(TemplateTodo templateTodo);
+    int deleteOne(long templateTodoNo);
+    int deleteUnSave(int userNo);
     int deleteTemplate(long templateNo);
 }
