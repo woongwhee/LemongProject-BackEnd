3d54dda7d780a2f0f99d0ec1e8baa7c5307da880
SongJiho1998 <106515028+SongJiho1998@users.noreply.github.com>
2023-01-26 19:20:50 +0900

    밀고 다시

--- src/main/java/site/lemongproject/web/member/controller/MemberController.java
+++ src/main/java/site/lemongproject/web/member/controller/MemberController.java
@@ -1,28 +1,27 @@
 package site.lemongproject.web.member.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.RequestEntity;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import site.lemongproject.common.response.ResponseBody;
 import site.lemongproject.common.response.ResponseBuilder;
 import site.lemongproject.common.util.FileUtil;
+import site.lemongproject.web.member.model.dto.MyProfileVo;
 import site.lemongproject.web.member.model.service.MemberService;
 import site.lemongproject.web.member.model.vo.Member;
 import site.lemongproject.web.member.model.vo.Profile;
 import site.lemongproject.web.photo.model.vo.Photo;
 
 import javax.servlet.http.HttpSession;
-import java.sql.Date;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 @RestController
+@RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
 
@@ -32,7 +31,7 @@ public class MemberController {
     final private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     // 로그인
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseBody<Member> loginMember(@RequestBody Member m, HttpSession session) {
         /*  @RequestBody(클라이언트-서버) -> json 객체를 자바 객체로 변환해줌
             HTTP 요청의 바디 내용을 통째로 자바객체로 변환해서 매핑된 메소드 파라미터로 전달
@@ -63,8 +62,6 @@ public class MemberController {
 //            return ResponseBuilder.unLogin(null);
 //        }
 //        System.out.println(loginUser);
-
-
         // 암호화 후
         if(loginUser != null && bCryptPasswordEncoder.matches(m.getUserPwd(), loginUser.getUserPwd())) {
             System.out.println("컨트롤러 넘어옴");
@@ -116,81 +113,148 @@ public class MemberController {
         }
     }
 
-    // 리액트 <-> 스프링 연동 TEST(성공)
-    @GetMapping("/hello")
-    public List<String> Hello(){
-        return Arrays.asList("Lemong_Project 백엔드 서버 : 8081", "Lemong_Project 프론트 서버 : 3000");
-    }
 
-    // 마이페이지 회원정보 수정
-    @GetMapping("/selectPro")
-    public List<Profile> MyPageUpdate(){
-
-        List<Profile> mList = memberService.selectMyProList();
-
-        return mList;
-    }
-
-    // MEMBER테이블 유저 정보 조회.
-    @GetMapping("/selectUser")
-    public List<Member> selectUser(){
-        List<Member> mList = memberService.selectUser();
-        ResponseBody<List<Member>> r= ResponseBuilder.success(mList);
-        return mList;
-    }
-
-    // USER_PROFILE테이블 유저 닉네임 업데이트.
-    @GetMapping("/checkNickName")
-    public int updateUser(@RequestParam(value="modifyNickname" , required=false) String nickName){
-        int upList = memberService.updateUser(nickName);
-
-        System.out.println(nickName);
-
-        return upList;
-    }
-
-    // USER_PROFILE테이블 유저 자기소개 업데이트.
-    @GetMapping("/updateComment")
-    public int updateComment(@RequestParam(value="modifyComment" , required = false) String comment){
-    // 이메일 전송
-//    @PostMapping("join/chEmail")
-//    public ResponseBody<Member> chEmail(@RequestBody Member m) {
-//        int result = memberService.checkEmail(m);
-//        System.out.print(result);
-//        if(result > 0) {
-//            return ResponseBuilder.success(result);
-//        } else {
-//            return ResponseBuilder.errorChEmail(result);
-//        }
-//    }
-
-
-        int upList2 = memberService.updateComment(comment);
-
-        System.out.println(comment);
-
-        return upList2;
-    }
-
-    @GetMapping("/myPwdUpdate")
-    public int myupdatePwd(@RequestParam(value="upPwd" , required = false)String upPwd){
-        int result = memberService.myupdatePwd(upPwd);
-        return result;
-    }
 
     // 유저 프로필 INSERT. => 웅휘형이 만든 FileUtil로 빼기 => rename(m.getOriginalFilename()) 오류 고치기.
     @RequestMapping(value="/insertUserProfile")
 //    @RequestMapping(value="/insertUserProfile", method=RequestMethod.POST)
-    public ResponseBody<Photo> insertUserProfile(
-            @RequestParam(value="file", required=false) MultipartFile[] files) {
+    public int insertUserProfile(
+            @RequestParam(value="file", required=false) MultipartFile[] files){
+
+        String webPath = "/resources/images/userProfile/";
+
+        int result = 0;
 
         Photo p = new Photo();
-        p.setUserNo(2);
-        fileUtil.saveFile(files[0], p);
-        int result = memberService.insertUserProfile(p);
-//        if(result>0){
-//        }
-        return ResponseBuilder.success(p);
+
+        for (MultipartFile mf : files) {
+
+            String originFileName = mf.getOriginalFilename(); // 원본 파일 명
+            long fileSize = mf.getSize(); // 파일 사이즈
+            System.out.println("originFileName : " + originFileName);
+            System.out.println("fileSize : " + fileSize);
+
+            // 1. 원본 파일명 뽑기.
+            String originName= mf.getOriginalFilename();
+
+            // 2. 시간 형식을 문자열로 뽑아오기.
+            // 년월일시분초
+            String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+
+            // 3. 뒤에 붙을 5자리 랜덤값 뽑기.
+            int random = (int)(Math.random() * 90000 + 10000); // 5자리 랜덤값
+
+            // 4. 원본파일명으로부터 확장자명 뽑기.
+            // .jpg
+            String ext = originName.substring(originName.lastIndexOf("."));
+
+            // 5. 다 이어붙이기.
+            String changeName = currentTime + random + ext;
+
+            // 폴더에 이미지 저장.
+            String savePath = "C:/LemongProject/src/main/webapp/resources/images/userProfile/";
+
+            if (!mf.getOriginalFilename().equals("")) { // 파일명이 비어있지 않은 경우
+                try {
+                    mf.transferTo(new File(savePath + changeName));
+                } catch (IllegalStateException | IOException e) {
+                    System.out.println(e.getMessage() + "오류 발생");
+                }
+            }
+
+            p.setFilePath(webPath+mf.getOriginalFilename());
+            p.setUserNo(2);
+            p.setChangeName(changeName);
+            p.setOriginName(mf.getOriginalFilename());
+
+            result = memberService.insertUserProfile(p);
+
+        }
+        return result;
     }
 
+    // 유저 프로필 UPDATE. => 웅휘형이 만든 FileUtil로 빼기 => rename(m.getOriginalFilename()) 오류 고치기.
+    @RequestMapping(value = "/updateUserProfile")
+    public int updateUserProfile(
+            @RequestParam(value = "file" , required = false) MultipartFile[] ufiles) {
+
+        System.out.println(ufiles + "통과됨");
+        
+        String webPath = "/resources/images/userProfile/";
+
+        int result = 0;
+
+        Photo p = new Photo();
+
+        for (MultipartFile mf : ufiles) {
+
+            String originFileName = mf.getOriginalFilename(); // 원본 파일 명
+            long fileSize = mf.getSize(); // 파일 사이즈
+            System.out.println("originFileName : " + originFileName);
+            System.out.println("fileSize : " + fileSize);
+
+            // 1. 원본 파일명 뽑기.
+            String originName= mf.getOriginalFilename();
+
+            // 2. 시간 형식을 문자열로 뽑아오기.
+            // 년월일시분초
+            String currentTime = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+
+            // 3. 뒤에 붙을 5자리 랜덤값 뽑기.
+            int random = (int)(Math.random() * 90000 + 10000); // 5자리 랜덤값
+
+            // 4. 원본파일명으로부터 확장자명 뽑기.
+            // .jpg
+            String ext = originName.substring(originName.lastIndexOf("."));
+
+            // 5. 다 이어붙이기.
+            String changeName = currentTime + random + ext;
+
+            // 폴더에 이미지 저장.
+            String savePath = "C:/LemongProject/src/main/webapp/resources/images/userProfile/";
+
+            if (!mf.getOriginalFilename().equals("")) { // 파일명이 비어있지 않은 경우
+                try {
+                    mf.transferTo(new File(savePath + changeName));
+                } catch (IllegalStateException | IOException e) {
+                    System.out.println(e.getMessage() + "오류 발생");
+                }
+            }
+                
+            p.setFilePath(webPath+mf.getOriginalFilename());
+            p.setUserNo(2);
+            p.setChangeName(changeName);
+            p.setOriginName(mf.getOriginalFilename());
+
+            result = memberService.updateUserProfile(p);
+
+        }
+        return result;
+
     }
+
+    @GetMapping("/selectMyProfileImg")
+    // 프로필 사진 뽑아서 리액트로 보내기
+    public List<Photo> selectMyProfile(){
+
+        List<Photo> pList = memberService.selectMyProfile();
+
+        return pList;
+    }
+
+    @GetMapping("/myprofile")
+    public ResponseBody<MyProfileVo> getLogin(@SessionAttribute("loginUser") Member loginMember){
+
+//        List<Member> mList = memberService.userSelect(loginMember);
+        MyProfileVo glv=memberService.getMyProfile(loginMember.getUserNo());
+        if(glv!=null) {
+            return ResponseBuilder.success(glv);
+        }else
+        {
+            return ResponseBuilder.success(glv);
+        }
+        }
+
+
+
+}
