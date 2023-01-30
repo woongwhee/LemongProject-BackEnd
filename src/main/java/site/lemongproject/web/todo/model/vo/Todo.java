2d08a1ac769fc20ae4bdc472b625d7c38f15acb0
pinesolhan <carpe.sol.sun@gmail.com>
2023-01-30 21:49:08 +0900

    투두리스트 작성, 삭제, 일정완료

--- src/main/java/site/lemongproject/web/todo/model/vo/Todo.java
+++ src/main/java/site/lemongproject/web/todo/model/vo/Todo.java
@@ -1,15 +1,18 @@
 package site.lemongproject.web.todo.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.sql.Date;
+//시/분/초 값을 가져오려면 java.util.Date로
 
 @Data
 public class Todo {
 
     private long todoNo; //투두넘버
 
-    private Date todoDate; //투두리스트 날짜
+    //@JsonFormat(pattern = "yyMMdd" ,timezone = "GMT+9") //JSON의 165030033같은 이상한 날짜값을 제대로 변환시켜준다.
+    private String todoDate; //투두리스트 날짜
 
     private int userNo; //작성자
 
