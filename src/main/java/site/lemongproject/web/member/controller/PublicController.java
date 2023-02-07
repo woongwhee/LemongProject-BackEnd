c4feaf500f41710bbbbd4adc0a221abec730c368
minseok <mins5714@gmail.com>
2023-02-07 13:08:57 +0900

    2.05

--- src/main/java/site/lemongproject/web/member/controller/PublicController.java
+++ src/main/java/site/lemongproject/web/member/controller/PublicController.java
@@ -29,7 +29,7 @@ public class PublicController {
     final private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     // 로그인
-    @PostMapping("login")
+    @PostMapping("/login")
     public ResponseBody<Member> loginMember(@RequestBody Member m, HttpSession session) {
         /*  @RequestBody(클라이언트-서버) -> json 객체를 자바 객체로 변환해줌
             HTTP 요청의 바디 내용을 통째로 자바객체로 변환해서 매핑된 메소드 파라미터로 전달
