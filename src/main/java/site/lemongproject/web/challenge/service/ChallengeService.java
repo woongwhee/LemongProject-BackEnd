f46bbfe986cc024c12b5d83f287657d4803b87fc
97minseok <mins5714@gmail.com>
2023-02-16 17:36:51 +0900

    2.16

--- src/main/java/site/lemongproject/web/challenge/service/ChallengeService.java
+++ src/main/java/site/lemongproject/web/challenge/service/ChallengeService.java
@@ -5,7 +5,10 @@ import site.lemongproject.web.challenge.model.dto.Challenge;
 import site.lemongproject.web.challenge.model.dto.ChallengeChat;
 import site.lemongproject.web.challenge.model.dto.ChallengeTodo;
 import site.lemongproject.web.challenge.model.dto.ChallengeUser;
-import site.lemongproject.web.challenge.model.vo.*;
+import site.lemongproject.web.challenge.model.vo.ChallengeListVo;
+import site.lemongproject.web.challenge.model.vo.MultiCreateVo;
+import site.lemongproject.web.challenge.model.vo.ChallengeUserVo;
+import site.lemongproject.web.challenge.model.vo.SingleStartVo;
 
 import java.util.List;
 
@@ -33,10 +36,4 @@ public interface ChallengeService {
     List<ChallengeTodo> calChTodo(ChallengeTodo ct);
 
     List<ChallengeListVo> getList(int page);
-
-    int clearTodo(TodoClearVo clearVo);
-
-    ChallengeDetailVo getDetail(int challengeNo);
-
-    List<ChallengeUser> myChallengeList(ChallengeUser u);
 }
