aa89883c983a6869464e240741a03c286820ce55
진웅휘 <110293650+woongwhee@users.noreply.github.com>
2023-02-21 17:20:07 +0900

    로그아웃 변경

--- src/main/java/site/lemongproject/web/member/controller/MemberController.java
+++ src/main/java/site/lemongproject/web/member/controller/MemberController.java
@@ -40,9 +40,9 @@ public class MemberController {
 
     // USER_PROFILE테이블 유저 닉네임 업데이트.
     @GetMapping("/checkNickName")
-    public int updateUser(@SessionAttribute("loginUser")Member loginUser,@RequestParam(value="modifyNickname" , required=false) String nickName){
+    public int updateUser(@SessionAttribute("loginUser") Member loginUser, @RequestParam(value = "modifyNickname", required = false) String nickName) {
 
-        Profile p=new Profile();
+        Profile p = new Profile();
         p.setUserNo(loginUser.getUserNo());
         p.setNickName(nickName);
         int upList = memberService.updateProfile(p);
@@ -54,8 +54,8 @@ public class MemberController {
 
     // USER_PROFILE테이블 유저 자기소개 업데이트.
     @GetMapping("/updateComment")
-    public int updateComment(@SessionAttribute("loginUser")Member loginUser,@RequestParam(value="modifyComment" , required = false) String comment){
-        Profile p=new Profile();
+    public int updateComment(@SessionAttribute("loginUser") Member loginUser, @RequestParam(value = "modifyComment", required = false) String comment) {
+        Profile p = new Profile();
         p.setUserNo(loginUser.getUserNo());
         p.setProfileComment(comment);
         int upList2 = memberService.updateProfile(p);
@@ -63,21 +63,21 @@ public class MemberController {
     }
 
     @GetMapping("/myPwdUpdate")
-    public int myupdatePwd(@SessionAttribute("loginUser") Profile loginUser , @RequestParam(value = "updatePwd" , required = false)String updatePwd){
+    public int myupdatePwd(@SessionAttribute("loginUser") Profile loginUser, @RequestParam(value = "updatePwd", required = false) String updatePwd) {
         String pwd = bCryptPasswordEncoder.encode(updatePwd);
         int userNo = loginUser.getUserNo();
 
         System.out.println(updatePwd + " === success === ");
-        ChangePwdVo cpw = new ChangePwdVo(userNo,pwd);
+        ChangePwdVo cpw = new ChangePwdVo(userNo, pwd);
         int result = memberService.updatePassword(cpw);
         return result;
     }
 
     // 유저 프로필 INSERT. => 웅휘형이 만든 FileUtil로 빼기 => rename(m.getOriginalFilename()) 오류 고치기.
 //    @PostMapping("/insertUserProfile")
-    @RequestMapping(value="/insertUserProfile", method=RequestMethod.POST)
+    @RequestMapping(value = "/insertUserProfile", method = RequestMethod.POST)
     public ResponseBody<Photo> insertProfilePhoto(
-            @RequestParam(value="file", required=false) MultipartFile[] files,
+            @RequestParam(value = "file", required = false) MultipartFile[] files,
             @SessionAttribute("loginUser") Profile loginUser) {
         Photo p = new Photo();
         p.setUserNo(loginUser.getUserNo());
@@ -86,16 +86,17 @@ public class MemberController {
         return ResponseBuilder.success(result);
 
     }
+
     // 유저 프로필 UPDATE. => 웅휘형이 만든 FileUtil로 빼기 => rename(m.getOriginalFilename()) 오류 고치기.
     @RequestMapping(value = "/updateUserProfile")
     public int updateUserProfile(
-            @RequestParam(value = "file" , required = false) MultipartFile[] ufiles ,
+            @RequestParam(value = "file", required = false) MultipartFile[] ufiles,
             @SessionAttribute(value = "loginMember") Member loginMember) {
 
-        Photo p=new Photo();
+        Photo p = new Photo();
         p.setUserNo(loginMember.getUserNo());
-        fileUtil.saveFile(ufiles[0],p );
-        int result=memberService.insertUserPhoto(p);
+        fileUtil.saveFile(ufiles[0], p);
+        int result = memberService.insertUserPhoto(p);
 
 
         return result;
@@ -103,35 +104,34 @@ public class MemberController {
     }
 
     @GetMapping("/myprofile")
-    public ResponseBody<MyProfileVo> getLogin(@SessionAttribute("loginUser") Member loginMember){
+    public ResponseBody<MyProfileVo> getLogin(@SessionAttribute("loginUser") Member loginMember) {
 
 //        List<Member> mList = memberService.userSelect(loginMember);
-        MyProfileVo glv=memberService.getMyProfile(loginMember.getUserNo());
-        if (glv!=null) {
+        MyProfileVo glv = memberService.getMyProfile(loginMember.getUserNo());
+        if (glv != null) {
             return ResponseBuilder.success(glv);
-        }else
-        {
+        } else {
             return ResponseBuilder.success(glv);
         }
     }
     //
 
     @GetMapping("/selectMember")
-    public ResponseBody<Member> selectMember(@SessionAttribute(value = "loginUser") Profile loginUser){
+    public ResponseBody<Member> selectMember(@SessionAttribute(value = "loginUser") Profile loginUser) {
         Member m = memberService.selectMembers(loginUser.getUserNo());
         return ResponseBuilder.success(m);
     }
 
     // userNo에 해당하는 user 프로필 정보 가져오기(changeName 포함).
     @GetMapping("/selectMyProfile")
-    public ResponseBody<Profile> selectMyProfile(@RequestParam(value = "userNo" , required = false) int userNo){
+    public ResponseBody<Profile> selectMyProfile(@RequestParam(value = "userNo", required = false) int userNo) {
         Profile p = memberService.selectMyProfile(userNo);
         return ResponseBuilder.success(p);
     }
 
     // 검색 기능(유저 아이디 검색)
     @GetMapping("/searchUser")
-    public ResponseBody<Profile> searchUser(@RequestParam(value="userNick" , required = false) String userNick){
+    public ResponseBody<Profile> searchUser(@RequestParam(value = "userNick", required = false) String userNick) {
 
         System.out.println(userNick + " : success");
 
@@ -141,33 +141,13 @@ public class MemberController {
 
         return ResponseBuilder.success(p);
     }
+
     @GetMapping("/logout")
-    public ResponseBody<Integer> logout(@SessionAttribute("loginUser")Profile profile, @SessionAttribute("socialType")SocialType socialType, HttpSession session){
+    public ResponseBody<Integer> logout(@SessionAttribute("loginUser") Profile profile, @SessionAttribute("socialType") SocialType socialType, HttpSession session) {
+            session.invalidate();
+            return ResponseBuilder.success(socialType);
 
-        String type = "";
-
-        switch (socialType){
-            case NONE:
-                session.invalidate();
-                break;
-            case NAVER:
-                session.invalidate();
-                break;
-            case KAKAO:
-                session.invalidate();
-//                memberService.kakaoLogout();
-                System.out.println("카카오 자체 로그아웃 완료");
-                break;
-                // 카카오 연결 끊는 링크 달아두기(별표)
-        }
-//        System.out.println(type);
-        if(type == "KAKAO") {
-            return ResponseBuilder.logoutKakao(type);
-        } else {
-            return ResponseBuilder.success(type);
-        }
     }
-
 
 
     @GetMapping("/deleteUser")
@@ -192,7 +172,7 @@ public class MemberController {
                 break;
         }
 
-        if(result > 0) {
+        if (result > 0) {
             session.invalidate();
             return ResponseBuilder.success(1);
         } else {
@@ -201,45 +181,45 @@ public class MemberController {
     }
 
 
+    // 유효성 검사 통과 후 닉네임 변경.
+    @GetMapping("/updateMyNick")
+    public ResponseBody<Profile> updateMyNick(@RequestParam(value = "updateNick", required = false) String updateNick,
+                                              @RequestParam(value = "userNo", required = false) int userNo) {
 
-        // 유효성 검사 통과 후 닉네임 변경.
-        @GetMapping("/updateMyNick")
-        public ResponseBody<Profile> updateMyNick(@RequestParam(value = "updateNick" , required = false) String updateNick ,
-        @RequestParam(value = "userNo" , required = false) int userNo){
+        System.out.println(userNo + " === updateMyNick === ");
+        System.out.println(updateNick + " === updateMyNick === ");
 
-            System.out.println(userNo + " === updateMyNick === ");
-            System.out.println(updateNick + " === updateMyNick === ");
+        Profile pro = new Profile();
+        pro.setUserNo(userNo);
+        pro.setNickName(updateNick);
 
-            Profile pro = new Profile();
-            pro.setUserNo(userNo);
-            pro.setNickName(updateNick);
+        Profile p = memberService.updateMyNick(pro);
 
-            Profile p = memberService.updateMyNick(pro);
+        return ResponseBuilder.success(p);
+    }
 
-            return ResponseBuilder.success(p);
-        }
-
-        // 마이페이지 자기소개 변경
-        @GetMapping("/updateMyContent")
-        public ResponseBody<Profile> updateMyContent(@RequestParam(value = "updateCont" , required = false) String updateCont ,
-        @RequestParam(value = "userNo" , required = false) int userNo){
-            Profile p = new Profile();
-            p.setProfileComment(updateCont);
-            p.setUserNo(userNo);
-            Profile content = memberService.updateMyContent(p);
-            return ResponseBuilder.success(content);
-        }
+    // 마이페이지 자기소개 변경
+    @GetMapping("/updateMyContent")
+    public ResponseBody<Profile> updateMyContent(@RequestParam(value = "updateCont", required = false) String updateCont,
+                                                 @RequestParam(value = "userNo", required = false) int userNo) {
+        Profile p = new Profile();
+        p.setProfileComment(updateCont);
+        p.setUserNo(userNo);
+        Profile content = memberService.updateMyContent(p);
+        return ResponseBuilder.success(content);
+    }
 
     // 마이페이지에서 유저 닉네임 검색해서 없는 닉네임인 경우 변경
     @GetMapping("/MyPageNickCheck")
-    public ResponseBody<Profile> MyPageNickCheck(@RequestParam(value = "checkNick" , required = false) String checkNick){
+    public ResponseBody<Profile> MyPageNickCheck(@RequestParam(value = "checkNick", required = false) String checkNick) {
 
         Profile p = memberService.MyPageNickCheck(checkNick);
 
         return ResponseBuilder.success(p);
     }
+
     @GetMapping("/getSocialType")
-    public ResponseBody<SocialType> getSocialType(@SessionAttribute("socialType") SocialType socialType){
+    public ResponseBody<SocialType> getSocialType(@SessionAttribute("socialType") SocialType socialType) {
         return ResponseBuilder.success(socialType);
     }
 }
